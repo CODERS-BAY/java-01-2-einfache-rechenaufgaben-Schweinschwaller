1a29623955ccb59e7bfe437fddfd4c6bea65e237
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-08-27 11:18:44 +0200

    Add * and /

--- src/SimpleCalculations.java
+++ src/SimpleCalculations.java
@@ -13,14 +13,22 @@ public class SimpleCalculations {
         a = 1.23;
         b = 3.21;
 
-        System.out.printf("%d + %d = %4d", x, y, (x + y));
+        System.out.printf("%d + %d = %d", x, y, (x + y));
         System.out.println();
-        System.out.printf("%d - %d = %4d", x, y, (x - y));
+        System.out.printf("%d - %d = %d", x, y, (x - y));
+        System.out.println();
+        System.out.printf("%d * %d = %d", x, y, (x * y));
+        System.out.println();
+        System.out.printf("%d / %d = %d", x, y, (x / y));
 
         System.out.println();
         System.out.println();
         System.out.printf("%.2f + %.2f = %.2f", a, b, (a + b));
         System.out.println();
         System.out.printf("%.2f - %.2f = %.2f", a, b, (a - b));
+        System.out.println();
+        System.out.printf("%.2f * %.2f = %.2f", a, b, (a * b));
+        System.out.println();
+        System.out.printf("%.2f / %.2f = %.2f", a, b, (a / b));
     }
 }
